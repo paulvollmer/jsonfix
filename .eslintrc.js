476ad1c6ae81e963ed2eb4866bcb2b2a56294828
Paul Vollmer <paul.vollmer@protonmail.com>
2018-01-25 16:19:43 +0100

    Eslint changed config, deleted old Gruntfile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-  "extends": "google"
+  "extends": ["eslint:recommended", "google"],
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "env" : {
+    "node" : true,
+    "mocha": true
+  },
+  "rules": {
+    "strict": "warn",
+    "max-len": ["error", 120]
+  }
 };
