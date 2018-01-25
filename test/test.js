dbf05293e345e9c2e892aec4e29314c8ff346775
Paul Vollmer <paul.vollmer@protonmail.com>
2018-01-25 17:46:08 +0100

    Cleanup testing

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ let JSONFix = require('../jsonfix');
 
 describe('JSONFix', function() {
   describe('empty data', function() {
-    it('testing empty data', function() {
+    it('data:', function() {
       let result = JSONFix.process('');
       assert.deepEqual(result, {});
       assert.equal(JSONFix.isValid, false);
@@ -14,8 +14,9 @@ describe('JSONFix', function() {
   });
 
   describe('valid data', function() {
-    it('testing valid data', function() {
-      let result = JSONFix.process('{"hello": "world"}');
+    const testData = '{"hello": "world"}';
+    it(`data: ${testData}`, function() {
+      let result = JSONFix.process(testData);
       assert.deepEqual(result, {'hello': 'world'});
       assert.equal(JSONFix.isValid, true);
       assert.equal(JSONFix.wasFixed, false);
@@ -23,75 +24,113 @@ describe('JSONFix', function() {
     });
   });
 
-  describe('testing data with error got "EOF"', function() {
-    it('data: {', function() {
-      let result = JSONFix.process('{');
-      assert.deepEqual(result, {});
+  describe('broken data', function() {
+    describe('with error got "EOF"', function() {
+      const testData1 = '{';
+      it(`data: ${testData1}`, function() {
+        let result = JSONFix.process(testData1);
+        assert.deepEqual(result, {});
+        assert.equal(JSONFix.isValid, false);
+        assert.equal(JSONFix.wasFixed, true);
+      });
+
+      const testData2 = '{"hello": "world"';
+      it(`data: ${testData2}`, function() {
+        let result = JSONFix.process(testData2);
+        assert.deepEqual(result, {hello: 'world'});
+        assert.equal(JSONFix.isValid, false);
+        assert.equal(JSONFix.wasFixed, true);
+      });
+
+      const testData3 = '[';
+      it(`data: ${testData3}`, function() {
+        let result = JSONFix.process(testData3);
+        assert.deepEqual(result, []);
+        assert.equal(JSONFix.isValid, false);
+        assert.equal(JSONFix.wasFixed, true);
+      });
+
+      const testData4 = '[0, 1, 2';
+      it(`data: ${testData4}`, function() {
+        let result = JSONFix.process(testData4);
+        assert.deepEqual(result, [0, 1, 2]);
+        assert.equal(JSONFix.isValid, false);
+        assert.equal(JSONFix.wasFixed, true);
+      });
     });
 
-    it('data: {"hello": "world"', function() {
-      let result = JSONFix.process('{"hello": "world"');
-      assert.deepEqual(result, {hello: 'world'});
+    // describe('missing colon', function() {
+    //   it('double colon', function() {
+    //     let result = JSONFix.process('{"foo":: "bar"}');
+    //     assert.deepEqual(result, {foo: 'bar'});
+    //     assert.equal(JSONFix.wasFixed, true);
+    //   });
+    // });
+
+    // describe('missing doublequote at the end', function() {
+    //   const testData = '{"foo": "bar}'
+    //   it(`data: ${testData}`, function() {
+    //     let result = JSONFix.process(testData);
+    //     assert.deepEqual(result, {foo: 'bar'});
+    //     assert.equal(JSONFix.isValid, false);
+    //     assert.equal(JSONFix.wasFixed, true);
+    //   });
+    // });
+
+    // describe('testing data with error got "undefined"', function() {
+    //   it('data:');
+    // });
+    //
+    // describe('testing data with error got "}"', function() {
+    //   it('data:');
+    // });
+    //
+    // describe('testing data with error got ":"', function() {
+    //   it('data:');
+    // });
+
+    describe('with error got "STRING"', function() {
+      const testData1 ='{"hello" "world"}';
+      it(`data: ${testData1}`, function() {
+        let result = JSONFix.process(testData1);
+        assert.deepEqual(result, {hello: 'world'});
+        assert.equal(JSONFix.isValid, false);
+        assert.equal(JSONFix.wasFixed, true);
+      });
+
+      const testData2 = '{\n"hello" "world",\n"foo" "bar"\n}';
+      it(`data: ${testData2}`, function() {
+        let result = JSONFix.process(testData2);
+        assert.deepEqual(result, {hello: 'world', foo: 'bar'});
+        assert.equal(JSONFix.isValid, false);
+        assert.equal(JSONFix.wasFixed, true);
+      });
     });
 
-    it('data: [', function() {
-      let result = JSONFix.process('[');
-      assert.deepEqual(result, []);
-    });
-
-    it('data: [0, 1, 2', function() {
-      let result = JSONFix.process('[0, 1, 2');
-      assert.deepEqual(result, [0, 1, 2]);
-    });
+    // describe('NUMBER', function() {
+    //   it('testing data with error got "NUMBER"', function() {
+    //     var result = JSONFix.process('[0 1');
+    //     assert.deepEqual(result, [0, 1]);
+    //   });
+    // });
   });
 
-  // describe('testing data with error got "undefined"', function() {
-  //   it('data:');
-  // });
-  //
-  // describe('testing data with error got "}"', function() {
-  //   it('data:');
-  // });
-  //
-  // describe('testing data with error got ":"', function() {
-  //   it('data:');
-  // });
 
-  describe('testing data with error got "STRING"', function() {
-    it('data: {"hello" "world"}', function() {
-      let result = JSONFix.process('{"hello" "world"}');
-      assert.deepEqual(result, {hello: 'world'});
-    });
-
-    it('data: {\n"hello" "world",\n"foo" "bar"\n}', function() {
-      let result = JSONFix.process('{\n"hello" "world",\n"foo" "bar"\n}');
-      assert.deepEqual(result, {hello: 'world', foo: 'bar'});
-    });
-  });
-
-  // describe('NUMBER', function() {
-  //   it('testing data with error got "NUMBER"', function() {
-  //     let result = JSONFix.process('[0 1');
-  //     assert.deepEqual(result, [0, 1]);
+  // describe('jsonlintErrorParser', function() {
+  //   it('parse an jsonlint error', function() {
+  //     var errorMessage = "Parse error on line 1: \n\
+  //   \n\
+  //   ^\n\
+  //   Expecting 'STRING', 'NUMBER', 'NULL', 'TRUE', 'FALSE', '{', '[', got 'EOF'";
+  //     var result = jsonlintErrorParser(errorMessage);
+  //     var expected = {
+  //       description: "Parse error",
+  //       line: "1",
+  //       code: "",
+  //       expecting: "'STRING', 'NUMBER', 'NULL', 'TRUE', 'FALSE', '{', '['",
+  //       got: "EOF'"
+  //     };
+  //     assert.deepEqual(result, expected, "Passed!");
   //   });
   // });
 });
-
-
-// describe('jsonlintErrorParser', function() {
-//   it('parse an jsonlint error', function() {
-//     var errorMessage = "Parse error on line 1: \n\
-//   \n\
-//   ^\n\
-//   Expecting 'STRING', 'NUMBER', 'NULL', 'TRUE', 'FALSE', '{', '[', got 'EOF'";
-//     let result = jsonlintErrorParser(errorMessage);
-//     var expected = {
-//       description: "Parse error",
-//       line: "1",
-//       code: "",
-//       expecting: "'STRING', 'NUMBER', 'NULL', 'TRUE', 'FALSE', '{', '['",
-//       got: "EOF'"
-//     };
-//     assert.deepEqual(result, expected, "Passed!");
-//   });
-// });
